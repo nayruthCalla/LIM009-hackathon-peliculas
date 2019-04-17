8e4a6f40c26662bd024fc82cb555db5446971fc4
DianaOropeza <diana94karina@hotmail.com>
2019-04-17 09:26:14 -0500

    boton va hacia arriba

--- src/data.js
+++ src/data.js
@@ -9,3 +9,21 @@ const sortData = (data, sortBy, sortOrder) => {
 window.GlobalData = {
   sortData
 };
+
+$(document).ready(function () {
+    
+  /*********************************************** boton hacia arriba **********************************************/
+  $('.ir-arriba').click(function(){
+      $('body, html').animate({
+          scrollTop: '0px'
+      }, 1000);
+  });
+
+  $(window).scroll(function(){
+      if( $(this).scrollTop() > 0 ){
+          $('.ir-arriba').slideDown(600);
+      } else {
+          $('.ir-arriba').slideUp(600);
+      }
+  });
+});
