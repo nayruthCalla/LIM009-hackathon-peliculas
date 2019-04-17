2a069225d6abaaadecb34a4ff475cfaa96520d7b
nayruthCalla <nayruth.calla@gmail.com>
2019-04-16 23:12:59 -0500

    estilos css

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ uniqueData.addEventListener("keydown", e => {
 
 searchTitle.addEventListener("click", () => {
   let getTextSearch = document.getElementById("busqueda").value.trim();
+  Carruselimg.classList.add("hide");
   url = `${urlMain}s=${getTextSearch}${apikey}`;
   console.log(url);
   fetch(url)
@@ -30,11 +31,13 @@ searchTitle.addEventListener("click", () => {
 const template = valuesData => {
   let clear = " ";
   valuesData.forEach(view => {
-    clear += ` <div class=" text-center col-xs-8 col-sm-4 col-md-3 margin " style="width:300px; height:400px; border-style:solid;">
-    <h3 font-weight: bold">${view.Title}</h3>
-    <img src= ${view.Poster} style="width:160px; height:180px"></>
-    <h4>Año de Estreno:${view.Year}</h4>
-    <h4>Genero: ${view.Type}</h4>
+    clear += ` <div class="text-center col-xs-8 col-sm-4 col-md-3 margin " style="width:250px; overflow: hidden; height:400px;border-color:rgba(123, 185, 180, 0.384); border-style:solid;border-radius: 15px; box-shadow: 0px 4px 10px rgba(7, 1, 20, 0.747); ">
+    <h3 class='colo-text' font-weight: bold">${view.Title}</h3>
+    <img class="border" src= ${
+      view.Poster
+    } style="width:200px; height:180px"></>
+    <h4 class='colo-text'>Año de Estreno:${view.Year}</h4>
+    <h4 class='colo-text'>Genero: ${view.Type}</h4>
     <button>mas...+</button>  
     </div>`;
     document.getElementById("data-omdb").innerHTML = `${clear}`;
